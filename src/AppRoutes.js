d1656043a9f2be0edf894d5418cb2994cee6040b
Md Irbaaz Hussain <94373243+Irbaaz49@users.noreply.github.com>
2022-09-21 20:50:52 +0530

    Update AppRoutes.js

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,7 +1,17 @@
 import React from 'react';
-
+import Index from "./Pages/Index"
+import Home from "./Pages/Home"
+import NotFound from "./Pages/NotFound"
 export const AppRoutes = () => {
     return (
-
+<>
+  <Routes>
+        
+        <Route path="/" element={<Index></Index>} />
+        <Route path="/home" element={<Home></Home>}
+        <Route path="*" element={<NotFound></NotFound>}/ >
+        </Routes>
+        
+        </>
     )
 }

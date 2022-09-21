7ac37b743509871d868ce4cae8cd161f7221dba7
Md Irbaaz Hussain <94373243+Irbaaz49@users.noreply.github.com>
2022-09-21 20:59:39 +0530

    Update AppRoutes.js

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Index from "./Pages/Index"
 import Home from "./Pages/Home"
 import NotFound from "./Pages/NotFound"
+import { Routes, Route } from 'reacr-router-dom'
+
 export const AppRoutes = () => {
     return (
 <>
